37f6e26e8e7bbaa7fc42954390dc4bfebc63147e
Pavel Syunev <pavel.syunev@gmail.com>
2020-05-08 12:44:12 -0400

    Google auth

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieSesion = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+require("./models/User");
+require("./services/passport");
+
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+app.use(
+  cookieSesion({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 
