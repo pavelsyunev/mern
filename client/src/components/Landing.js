5f8812b6d66c96f4995f96337f20fe7791f612f7
Pavel Syunev <pavel.syunev@gmail.com>
2020-05-10 20:50:52 -0400

    Add Stripe payment process

---
+++ client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <div>
+      <h1>Mern</h1>
+      <p>Learn how to MERN works!</p>
+    </div>
+  );
+};
+
+export default Landing;
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieSesion = require("cookie-session");
 const passport = require("passport");
+const bodyParser = require("body-parser");
 const keys = require("./config/keys");
 require("./models/User");
 require("./services/passport");
@@ -13,6 +14,7 @@ mongoose.connect(keys.mongoURI, {
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
   cookieSesion({
     maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -24,6 +26,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require("./routes/authRoutes")(app);
+require("./routes/billingRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 
